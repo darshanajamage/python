09d1eb2da0d2c8f66573d6dc5080b4420ac82598
darshanajamage <darshanajamage@gmail.com>
2025-09-04 15:10:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import mymodule
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from mymodule import greet,add,sub,mul,div
+print(mymodule.greet("Darshana"))
+print(mymodule.add(10,20))
+print(mymodule.sub(37,19))
+print(mymodule.mul(13,28))
+print(mymodule.div(60,5))
---
+++ mymodule.py
@@ -0,0 +1,10 @@
+def greet(Darshana):
+    return f"Hello, {Darshana}!"
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
